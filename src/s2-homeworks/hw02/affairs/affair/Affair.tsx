4a3fb38743238cc3eb40b9b1c36f87d573a23719
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2022-10-28 16:28:15 +0300

    second homework

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -11,7 +11,7 @@ type AffairPropsType = {
 
 function Affair(props: AffairPropsType) {
     const deleteCallback = () => {
-        // need to fix
+        props.affair
     }
 
     const nameClass = s.name + ' ' + s2[props.affair.priority]
@@ -24,20 +24,18 @@ function Affair(props: AffairPropsType) {
             className={affairClass}
         >
             <div id={'hw2-name-' + props.affair._id} className={nameClass}>
-                {/*создаёт студент*/}
+                {props.affair.name}
 
-                {/**/}
             </div>
             <div id={'hw2-priority-' + props.affair._id} hidden>
-                {/*создаёт студент*/}
+                {props.affair.priority}
 
-                {/**/}
             </div>
 
             <button
                 id={'hw2-button-delete-' + props.affair._id}
                 className={buttonClass}
-                // need to fix
+
 
             >
                 {/*текст кнопки могут изменить студенты*/}
