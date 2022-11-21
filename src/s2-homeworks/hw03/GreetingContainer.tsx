2d9643bb95b3b8b06a74ec67dae49aae1ae8de18
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2022-11-21 15:03:55 +0300

    add commit tasks

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -42,7 +42,7 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
 
     const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
         setName(e.currentTarget.value) // need to fix
-        error && setError('Ошибка! Введите имя!')
+        error && setError('')
         // if(name.trim() === ''){
         //     setError('Ошибка! Введите имя!')
         // }else {
