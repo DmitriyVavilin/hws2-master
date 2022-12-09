fad44032edbf9c2dfae758e084afb0216bb2c188
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2022-12-09 13:20:33 +0300

    completed Routes

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import {Routes, Route, Navigate, NavLink} from 'react-router-dom'
+import {Route, Routes} from 'react-router-dom'
 import Error404 from './pages/Error404'
 import PreJunior from './pages/PreJunior'
 import Junior from './pages/Junior'
 import JuniorPlus from './pages/JuniorPlus'
-import {Sidebar} from "./sidebar/Sidebar";
-import {Header, PropsType} from "./header/Header";
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
@@ -22,9 +20,12 @@ function Pages() {
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
                 {/*<Route ...*/}
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>} />
-                <Route path={PATH.JUNIOR} element={<Junior/>} />
-                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>} />
+                <Route path={'/'} element={<PreJunior/>}>
+                    <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>} />
+                    <Route path={PATH.JUNIOR} element={<Junior/>} />
+                    <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>} />
+                </Route>
+
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
                 <Route path={'/*'} element={<Error404/>}/>
             </Routes>
