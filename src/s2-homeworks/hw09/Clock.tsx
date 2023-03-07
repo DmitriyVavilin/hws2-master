128e00e3243796cf0adcb53e099ca391847e89da
Dmitryi_Vavilin <dv.vavilin@mail.ru>
2023-03-07 14:12:24 +0300

    home_work 9 completed

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -51,7 +51,7 @@ function Clock() {
     const stringDate = getMonth.format(date) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = getDay.format(date).toUpperCase() || <br/> // пишут студенты
+    const stringDay = getDay.format(date) || <br/> // пишут студенты
     const stringMonth = getDate.format(date) || <br/> // пишут студенты
 
     return (
